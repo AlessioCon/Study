506e41bd49772a49f75d41b6c193f7f504b932c8
Alessio Conforto <alessioconforto20@gmail.com>
2022-08-10 16:12:32 +0200

    client test 1.9

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App'
+import env from "react-dotenv";
 
 
 console.log(process.env.URL_SERVER)
--- client/src/routers/Home.jsx
+++ client/src/routers/Home.jsx
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom";
-import env from "react-dotenv";
 
-import 'dotenv/config'
 
 
 function Home(){
@@ -9,7 +7,6 @@ function Home(){
         <main>
             <h1>Home</h1>
             <p>env = {env.URL_SERVER}</p>
-            <p>un altra prova = {process.env.URL_SERVER}</p>
             <Link to='/corsi'>i nostri corsi</Link>
         </main>
 
