e672c974c8b6bb42ecf159e3e8c7fb1d6b062259
Alessio Conforto <alessioconforto20@gmail.com>
2022-08-10 15:55:15 +0200

    client test 1.8

--- client/src/routers/Home.jsx
+++ client/src/routers/Home.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import env from "react-dotenv";
 
+import 'dotenv/config'
+
 
 function Home(){
     return(
         <main>
             <h1>Home</h1>
             <p>env = {env.URL_SERVER}</p>
+            <p>un altra prova = {process.env.URL_SERVER}</p>
             <Link to='/corsi'>i nostri corsi</Link>
         </main>
 
