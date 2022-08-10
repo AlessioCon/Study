3f945d845f07c7251566b133f8ec04e1be347fa0
Alessio Conforto <alessioconforto20@gmail.com>
2022-08-10 15:49:14 +0200

    client test 1.7

--- client/src/routers/Home.jsx
+++ client/src/routers/Home.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import env from "react-dotenv";
 
 
 function Home(){
     return(
         <main>
             <h1>Home</h1>
-            <p>env = {windos.env.URL_SERVER}</p>
+            <p>env = {env.URL_SERVER}</p>
             <Link to='/corsi'>i nostri corsi</Link>
         </main>
 
