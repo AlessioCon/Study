3ecc69ee847d9d24572be89c5486f9b89479f2b7
Alessio Conforto <alessioconforto20@gmail.com>
2022-08-11 13:03:43 +0200

    heroku test 1.2

--- server.js
+++ server.js
@@ -57,6 +57,10 @@ app.use(passport.session());
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname  , '/client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client" , "build" , "index.html"))
+    })
 }
 
 /*ONLINE IMPLEMENTATION*/
